8aa7e79d48fe3e4c7d5723845cd15520a1e00485
thanhluan2902 <you@example.com>
2020-07-08 21:29:16 +0700

    first commit

--- EX09/Bài 3/index.js
+++
@@ -1,9 +0,0 @@
-// function myfunction(){
-// 	var x = document.getElementsByClassName('w').value;
-// 	document.getElementsByClassName('info').style.fontSize = x + "px";
-// }
-
-var font = document.getElementsByClassName('font');
-function setFont(index,size){
-		document.getElementsByClassName('info')[index].style.fontSize = size;
-} 
