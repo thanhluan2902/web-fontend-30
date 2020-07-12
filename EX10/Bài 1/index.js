f60a96646aaf9eba137a6029ea1ee6e0a5fd53ee
thanhluan2902 <you@example.com>
2020-07-12 16:42:35 +0700

    first commit

---
+++ EX10/Bài 1/index.js
@@ -0,0 +1,79 @@
+var slider = document.querySelector('.slider');
+var buttons = document.querySelectorAll('.btn');
+var options = document.querySelectorAll('.option');
+var slides = document.querySelectorAll('.img');
+
+var index = 1;
+var opIndex = 0;
+var size = slides[index].clientWidth;
+
+update();
+
+function update(){
+	slider.style.transform = "translateX(" +(-size * index) + "px)";
+
+	options.forEach(op => op.classList.remove('colored'));
+	options[opIndex].classList.add('colored');
+
+}
+
+function slide(){
+	slider.style.transition = "transform .5s ease-in-out";
+	update();
+}
+
+function optionFunc(){
+	let i = Number(this.getAttribute('option-index'));
+	index = i + 1;
+	opIndex = i;
+	slide();
+}
+
+function btnCheck(){
+	if(this.id === "prev"){
+		index--;
+
+		if(opIndex == 0){
+			opIndex = 5;
+		}
+		else{
+			opIndex--;
+		}
+	}
+	else if(this.id === "next"){
+		index++;
+
+		if(opIndex == 5){
+			opIndex = 0;
+		}
+		else{
+			opIndex++;
+		}
+	}
+	
+	slide();
+}
+
+slider.addEventListener('transitionend', () => {
+	if(slides[index].id === "last"){
+		slider.style.transition = "none";
+		index = slides.length - 2;
+		slider.style.transform = "translateX(" +(-size * index) + "px)";
+	}
+
+	else if(slides[index].id === "first"){
+		slider.style.transition = "none";
+		index = 1;
+		slider.style.transform = "translateX(" +(-size * index) + "px)";
+	}
+});
+
+
+setInterval(function() {
+	slide();
+}, 2000);
+
+buttons.forEach(btn => btn.addEventListener('click', btnCheck));
+options.forEach(option => option.addEventListener('click', optionFunc));
+
+
---
+++ EX10/Bài 2/index.js
@@ -0,0 +1,78 @@
+
+function save(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var password = document.getElementById('password').value;
+    var rePassword = document.getElementById('rePassword').value;
+    // var trueCheck = document.getElementById('trueCheck');
+
+    if(_.isEmpty(name)){
+        document.getElementById('error').innerHTML = 'Uyen ngao ver lor!';
+        document.getElementById('trueCheck').style.display = "";
+    } else if(name.length < 9){
+        document.getElementById('error').innerHTML = 'Vui lang nhap > 8 ki tu!';
+        document.getElementById('trueCheck').style.display = "";
+    } else {
+        document.getElementById('error').innerHTML = '';
+        document.getElementById('trueCheck').style.display = "block";
+    }
+
+
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email !==''){
+        if (re.test(email) == false) 
+        {
+         document.getElementById('error-1').innerHTML = 'Email sai dinh dang kia ma!';
+         document.getElementById('trueCheck-1').style.display = "";
+     }else{
+         document.getElementById('error-1').innerHTML = '';
+         document.getElementById('trueCheck-1').style.display = "block";
+     }
+ }else{
+     document.getElementById('error-1').innerHTML = 'Vui long nhap email!';
+     document.getElementById('trueCheck-1').style.display = "";
+ }
+
+
+
+
+ var vnf_regex = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+ if(phone !==''){
+    if (vnf_regex.test(phone) == false) 
+    {
+     document.getElementById('error-2').innerHTML = 'Sai dinh dang kia ma!';
+     document.getElementById('trueCheck-2').style.display = "";
+ }else{
+     document.getElementById('error-2').innerHTML = '';
+     document.getElementById('trueCheck-2').style.display = "block";
+ }
+}else{
+ document.getElementById('error-2').innerHTML = 'Vui long nhap sdt!';
+ document.getElementById('trueCheck-2').style.display = "";
+}
+
+
+
+
+if(_.isEmpty(password)){
+    document.getElementById('error-3').innerHTML = 'Vui long nhap password!';
+    document.getElementById('trueCheck-3').style.display = "";
+}else {
+    document.getElementById('error-3').innerHTML = '';
+    document.getElementById('trueCheck-3').style.display = "block";
+}
+
+if(_.isEmpty(rePassword)){
+    document.getElementById('error-4').innerHTML = 'Vui long nhap lai password!';
+    document.getElementById('trueCheck-4').style.display = "";
+}
+else if(password != rePassword){
+    document.getElementById('error-4').innerHTML = 'ko trung khop!';
+    document.getElementById('trueCheck-4').style.display = "";
+}
+else {
+    document.getElementById('error-4').innerHTML = '';
+    document.getElementById('trueCheck-4').style.display = "block";
+}
+}
