a82a5dcfa2f0235b97d2909de403ebeef2504d4b
thanhluan2902 <you@example.com>
2020-08-20 20:37:54 +0700

    first commit

--- PHP-Buổi03/Bai2/detail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    echo 'mã sinh viên : '.$_GET['code']."<br>";
-    echo 'Họ và tên : '.$_GET['name']."<br>";
-    echo 'Số điện thoại : '.$_GET['sdt']."<br>";
-    echo 'Email : '.$_GET['email']."<br>";
-    echo 'Gioi tính : '.$_GET['gt']."<br>";
-    echo 'Địa chỉ : '.$_GET['address'];
-?>
